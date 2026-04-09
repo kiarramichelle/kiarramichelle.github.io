74a519646ff6c3e38ba8140a658299c9c451e0af
kiarramichelle <kiarramichelle.garcia@gmail.com>
2026-04-09 13:32:40 -0700

    Update site configuration for Kiarra Michelle

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
-  accentColor: "#1d4ed8",
+  name: "Kiarra Michelle",
+  title: "Cognitive Science grad & researcher",
+  description: "Portfolio website of Kiarra Michelle",
+  accentColor: "#8154eb",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "kiarramichelle.garcia@gmail.com",
+    linkedin: "https://www.linkedin.com/in/kiarra-garcia-186677212/",
+    twitter: "https://scholar.google.com/citations?hl=en&user=--DhUI8AAAAJ&view_op=list_works&gmla=AEk_c1s1KnjWTMcHusQ84F1BrKpm7iMpc1fLVKYw9SqUnmAYfLn4fJ0r-km46q7QonI_UNmdtoI2Cw83eaQoFy31",
+    github: "https://github.com/kiarramg",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
